1856ab132b398a8103ac3497232f82302080d84a
glebokhlopkov <glebka_90@mail.ru>
2023-11-09 16:52:26 +0300

    done post-view

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,8 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('email')
+        user_message = request.POST.get('message')
+        print(f'{name}: {user_message}')
     return render(request, 'catalog/contacts.html')
