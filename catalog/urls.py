7277ac409bc2c722268ea10501f66cd2d7d8d704
glebokhlopkov <glebka_90@mail.ru>
2023-11-23 15:05:40 +0300

    create tags correct html-code

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,6 @@ from catalog.views import home, contacts, product
 app_name = 'catalog'
 urlpatterns = [
     path('', home, name='home'),
-    path('product/', product, name='product'),
+    path('product/<int:pk>', product, name='product'),
     path('contacts/', contacts, name='contacts'),
 ]
