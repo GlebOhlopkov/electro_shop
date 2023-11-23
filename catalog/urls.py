4abab3511c3ce1d8eec061f8858cde590ea47d76
glebokhlopkov <glebka_90@mail.ru>
2023-11-23 13:26:39 +0300

    create tags correct html-code

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product
 
 app_name = 'catalog'
 urlpatterns = [
     path('', home, name='home'),
+    path('product/', product, name='product'),
     path('contacts/', contacts, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,11 +4,21 @@ from catalog.models import Product
 
 
 def home(request):
-    product_list = Product.objects.all()
+    product_list = Product.objects.all()[:6]
     context = {
-        'product_list': product_list
+        'product_list': product_list,
+        'title': 'Shop_Name',
     }
     return render(request, 'catalog/home.html', context)
+
+
+def product(request):
+    product_list = Product.objects.all()
+    context = {
+        'product_list': product_list,
+        'title': 'Product',
+    }
+    return render(request, 'catalog/product.html', context)
 
 
 def contacts(request):
@@ -16,4 +26,7 @@ def contacts(request):
         name = request.POST.get('email')
         user_message = request.POST.get('message')
         print(f'{name}: {user_message}')
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Contacts',
+    }
+    return render(request, 'catalog/contacts.html', context)
