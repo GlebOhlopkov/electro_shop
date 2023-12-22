e3710ac42e54b9953060de7e5d128f499059459c
glebokhlopkov <glebka_90@mail.ru>
2023-12-22 09:47:17 +0300

    correct forms by style

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,6 +8,11 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ('product_name', 'product_description', 'product_image', 'product_category', 'product_price',)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     def clean_product_name(self):
         banned_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
         cleaned_data = self.cleaned_data['product_name']
