7c240a86802dbe5d6ed8a1f68914f46ece3b9fcb
glebokhlopkov <glebka_90@mail.ru>
2024-01-06 16:22:39 +0300

    correct migrate

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('confirm_register/', EmailConfirmView.as_view(), name='confirm_register'),
-    path('confirm-email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
+    path('confirm_email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
     path('profile/', UserUpdateView.as_view(), name='profile'),
     path('profile/recovery', password_recovery, name='password_recovery'),
 ]
